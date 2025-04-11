b3f7185e0727c1b866a36b8c0e81b37ac66255e9
Diego Justino <diegojsilva01@outlook.com>
2025-04-11 20:26:09 -0300

    Button

--- app/src/main/java/com/example/electronator/MainActivity.java
+++ app/src/main/java/com/example/electronator/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.electronator;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Inflate the layout.xml
+                LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+                View layoutView = inflater.inflate(R.layout.layout, null);
+
+                // Set the inflated view as the content view
+                setContentView(layoutView);
+            }
+        });
     }
 }
